d1f4e574cea5cc2589f2d18e326ac12cd5fa9992
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2026-05-19 10:37:38 +0400

    repair tests

--- packages/devextreme/testing/tests/DevExpress.ui.widgets/toolbar.kbn.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets/toolbar.kbn.tests.js
@@ -565,7 +565,7 @@ QUnit.module('Widget interaction', {
         const dropDownButton = getDropDownButton(this.$element);
         assert.strictEqual(dropDownButton.option('opened'), true, 'popup opened');
 
-        const $listItem = dropDownButton._list.$element().find('.dx-list-item').first();
+        const $listItem = $(dropDownButton._list.$element().find('.dx-list-item').first());
         $listItem.trigger('dxclick');
         this.clock.tick(300);
 
@@ -587,7 +587,7 @@ QUnit.module('Widget interaction', {
         const dropDownButton = getDropDownButton(this.$element);
         assert.strictEqual(dropDownButton.option('opened'), true, 'popup opened');
 
-        const $listItem = dropDownButton._list.$element().find('.dx-list-item').first();
+        const $listItem = $(dropDownButton._list.$element().find('.dx-list-item').first());
         $listItem.get(0).focus();
         this.clock.tick(0);
 
@@ -606,7 +606,7 @@ QUnit.module('Widget interaction', {
         this.clock.tick(300);
 
         const dropDownButton = getDropDownButton(this.$element);
-        const $listItem = dropDownButton._list.$element().find('.dx-list-item').first();
+        const $listItem = $(dropDownButton._list.$element().find('.dx-list-item').first());
         $listItem.trigger('dxclick');
         this.clock.tick(300);
 
